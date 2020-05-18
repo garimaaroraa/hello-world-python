47b59b1444935638bb6bbb7626b4ff2cadf7ae9e
garimaarorashuttl <aroragarima0507@gmail.com>
2020-05-18 21:55:40 +0530

    Break test to see what happens

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -3,4 +3,4 @@ from hello_world.hello_world import hello_world
 
 def test_hello_world():
     name = "John"
-    assert f"Hello World {name}" == hello_world(name)
+    assert f"Hello World {name} " == hello_world(name)
